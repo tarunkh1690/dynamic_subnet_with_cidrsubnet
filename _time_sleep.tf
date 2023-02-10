ce2a6dc8085ec455d01bf0ef2f1d0dbdeafbe5a1
tarunkh1690 <51506748+tarunkh1690@users.noreply.github.com>
2023-02-11 00:10:06 +0530

    Create _time_sleep.tf

---
+++ _time_sleep.tf
@@ -0,0 +1,11 @@
+resource "time_sleep" "wait_30_seconds_public" {
+  depends_on = [aws_subnet.my-public-subnet]
+
+  create_duration = "10s"
+}
+
+resource "time_sleep" "wait_30_seconds_private" {
+  depends_on = [aws_subnet.my-private-subnet]
+
+  create_duration = "10s"
+}
